ff5965188c81e5895c64d3b816f9536bbaf744ef
amirulislambd <amirulislambd313@gmail.com>
2026-07-06 18:25:33 +0600

    design: added top margin to push GitHub Heatmap down

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -314,7 +314,7 @@ export default function AboutPage() {
         </div>
 
         {/* GitHub Heatmap */}
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 pb-20">
+        <div className="max-w-7xl mx-auto px-4 sm:px-6 mt-16 sm:mt-24 pb-20">
           <GitHubHeatmap />
         </div>
 
